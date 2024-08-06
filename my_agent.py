b1b7148ef2204028a43d3dc78a55c226235e3a9a
shjy2008 <shjy2011@163.com>
2024-08-06 11:33:00 +0800

    Add some comments

--- my_agent.py
+++ my_agent.py
@@ -14,53 +14,64 @@ class Node:
 
 class State:
     def __init__(self, bidding_on, items_left, my_cards, my_bank, opponents_bank, opponents_cards):
-        self.bidding_on = bidding_on
-        self.items_left = items_left
-        self.my_cards = my_cards
-        self.my_bank = my_bank
-        self.opponents_bank = opponents_bank
-        self.opponents_cards = opponents_cards
+        self.bidding_on = bidding_on    # The value of the item to bid on. e.g. 4
+        self.items_left = items_left    # The tuple of the items left. e.g. (-1, 1, 2, 3)
+        self.my_cards = my_cards        # The tuple of my cards. e.g. (1, 2, 3, 4)
+        self.my_bank = my_bank          # The total score of my bank. e.g. 2
+        self.opponents_bank = opponents_bank    # The total score of the opponent's bank. e.g. 1 (Only consider there's only one opponent)
+        self.opponents_cards = opponents_cards  # The tuple of the opponent's cards. e.g. (3, 4, 5, 6)
 
-        self.my_action = None
-        self.opponent_action = None
-    
+        # The value of my action in this round. Should be None on the start of a round
+        # Assigned after my action, and assigned None again after the opponent's move and the new round starts
+        self.my_action = None           
+   
+    # Evaluation function
     def get_evaluation_value(self):
         return self.my_bank - self.opponents_bank
     
+    # Check if it is the terminal state
     def is_terminal_state(self):
         return len(self.items_left) <= 1
     
+    # Get all possible next states on my round
     def get_my_possible_next_states(self):
         next_states = []
         for i in range(len(self.my_cards)):
+            # Get a new tuple of my cards after using a card
             my_action = self.my_cards[i]
-            next_my_cards = tuple([card for card in self.my_cards if card != my_action]) # Get a new tuple of my cards after using a card
-            # Generate a new state
+            next_my_cards = tuple([card for card in self.my_cards if card != my_action])
+
+            # Generate a new state. In this state, I finished my bid and wait for the opponent to bid
+            # (Assume the opponent bid after me and he know how much I bid, since it's the worse case)
             next_state = State(self.bidding_on, self.items_left[:], next_my_cards, self.my_bank, self.opponents_bank, self.opponents_cards)
             next_state.my_action = my_action
             next_states.append(next_state)
+
         return next_states
     
+    # Get all possible next states on the opponent's round(Assume the opponent's action is after mine and he know how much I bid. It's the worst case)
     def get_opponents_possible_next_states(self):
         next_states = []
         for i in range(len(self.opponents_cards)):
             opponent_action = self.opponents_cards[i]
             next_my_bank = self.my_bank
             next_opponents_bank = self.opponents_bank
-            next_bidding_on = self.items_left[0] # Note: In real cases, next item should be random
+            next_bidding_on = self.items_left[0] # Note: In real cases, next item should be random. Here I simply assume bidding the first item in the list
             next_items_left = self.items_left[1:]
             next_opponents_cards = tuple([card for card in self.opponents_cards if card != opponent_action]) # Get a new tuple of opponent's cards
+
+            # Modify my_bank or opponents_bank by checking who win this bidding. If tie, add to the next bid.
             if self.my_action > opponent_action:
                 next_my_bank += self.bidding_on
-            elif self.my_action == opponent_action:
-                next_bidding_on += self.bidding_on
-            else:
+            elif self.my_action < opponent_action:
                 next_opponents_bank += self.bidding_on
+            else:
+                next_bidding_on += self.bidding_on
             
-            # Generate a new state
-            new_state = State(next_bidding_on, next_items_left, self.my_cards[:], next_my_bank, next_opponents_bank, next_opponents_cards)
+            # Generate a new state. In this state, both sides finish bidding and a new round starts
+            next_state = State(next_bidding_on, next_items_left, self.my_cards[:], next_my_bank, next_opponents_bank, next_opponents_cards)
+            next_states.append(next_state)
 
-            next_states.append(new_state)
         return next_states
 
 
@@ -82,8 +93,10 @@ class RajAgent():
     -------
     AgentFunction(percepts)
         Returns the card value from hand to bid with
+
     """
-    
+
+    # Depth limit. 2 depth limits = 1 round. Don't enter odd number.
     DEPTH_LIMIT = 4
 
     def __init__(self, item_values, card_values):
@@ -95,12 +108,17 @@ class RajAgent():
         self.card_values = card_values
         self.item_values = item_values
 
+    # Recursively get the best child of a node
     def get_best_child(self, node, depth):
         state = node.state
         next_states = []
         is_max = depth % 2 == 0
+
+        # For Alpha-beta pruning
         alpha = -999999
         beta = 999999
+
+        # If is max, then it's my turn. Otherwise, it's the opponent's turn.
         if is_max:
             next_states = state.get_my_possible_next_states()
         else:
@@ -118,10 +136,11 @@ class RajAgent():
             else: # If not reaches the depth limit or terminal state, keep going deep
                 self.get_best_child(child_node, next_depth)
 
-            # Back up the value to parent
+            # Back up the value to parent if this child is better than the previous ones
             if node.value == None or (is_max and child_node.value > node.value) or ((not is_max) and child_node.value < node.value):
                 node.value = child_node.value
                 node.best_child = child_node
+                # Assign the new value of the parent to the alpha/beta value
                 if is_max:
                     alpha = max(alpha, node.value)
                 else:
@@ -160,16 +179,16 @@ class RajAgent():
         bank = percepts[3] # e.g. 2
         opponents_cards = percepts[4:] # e.g. ((3, 4, 5, 6),)
 
-         # Note: Only consider that there's only one opponent
-        root_state = State(bidding_on, items_left, my_cards, bank, 0, opponents_cards[0])
+        # Note: Only consider that there's only one opponent
+        # Note: Percepts don't provide the opponent's bank, but I can simply assume it's 0.
+        # I simply calculate (my_bank - opponent_bank) in the evaluation function, so it doesn't matter what opponent_bank originally is.
+        opponent_bank = 0
+        root_state = State(bidding_on, items_left, my_cards, bank, opponent_bank, opponents_cards[0])
         root_node = Node(root_state)
 
         best_child = self.get_best_child(root_node, 0)
         best_next_state = best_child.state
         action = best_next_state.my_action
-
-        # Currently this agent just bids the first card in its hand - you need to make it smarter!
-        # action = my_cards[0]
 
         # Return the bid
         return action
